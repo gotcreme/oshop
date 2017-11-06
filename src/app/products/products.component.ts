f2a015fc5db22d394703d0fa2219662159ae9fb9
gotcreme <193creme@gmail.com>
2017-11-07 00:58:51 +0200

    implemented products module

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from './../models/product';
+import { ProductService } from './../product.service';
+import { Component } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  category: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService) { 
+    productService
+      .getAll()
+      .switchMap(products => {
+        this.filteredProducts = this.products = products.map(p => new Product(p));
+        return route.queryParamMap;
+      })
+      .subscribe(params => {
+        this.category = params.get('category');
+        
+        this.filteredProducts = (this.category) ? 
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
   }
-
 }
